256e4c17ea4fcc0d1ac30a2229a668dfb3c19493
Juan Alvarez <nebiros@gmail.com>
2020-05-28 09:49:14 -0500

    Clean-up publisher example using azqueue broker

--- pub/go.mod
+++ pub/go.mod
@@ -3,7 +3,8 @@ module github.com/nebiros/poc-go-micro/pub
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.0
+	github.com/micro/cli/v2 v2.1.2
 	github.com/micro/go-micro/v2 v2.7.0
-	google.golang.org/protobuf v1.22.0
+	github.com/nebiros/poc-go-micro/broker/azqueue v0.0.0-20200528134027-4ec8aacec8bb
+	github.com/nebiros/poc-go-micro/server v0.0.0-20200528135749-bf67d010f2d6
 )
--- pub/main.go
+++ pub/main.go
@@ -2,26 +2,78 @@ package main
 
 import (
 	"context"
-	"log"
 	"time"
+
+	"github.com/nebiros/poc-go-micro/broker/azqueue"
+
+	"github.com/micro/go-micro/v2/client"
+
+	"github.com/micro/cli/v2"
+	"github.com/micro/go-micro/v2/config/cmd"
 
 	"github.com/micro/go-micro/v2"
 
-	proto "github.com/nebiros/poc-go-micro/pub/proto/pub"
+	log "github.com/micro/go-micro/v2/logger"
+	proto "github.com/nebiros/poc-go-micro/server/proto/example"
 )
 
+var (
+	storageAccountName string
+	storageAccountKey  string
+)
+
+func init() {
+	log.Init(log.WithLevel(log.Level(log.TraceLevel)))
+}
+
 func main() {
+	run()
+}
+
+func addCustomCmdFlags() {
+	app := cmd.App()
+	app.Flags = append(app.Flags, &cli.StringFlag{
+		Name:        "storage_account_name",
+		Usage:       "Storage account name",
+		EnvVars:     []string{"STORAGE_ACCOUNT_NAME"},
+		Required:    true,
+		Destination: &storageAccountName,
+	})
+	app.Flags = append(app.Flags, &cli.StringFlag{
+		Name:        "storage_account_key",
+		Usage:       "Storage account key",
+		EnvVars:     []string{"STORAGE_ACCOUNT_KEY"},
+		Required:    true,
+		Destination: &storageAccountKey,
+	})
+}
+
+func run() {
+	addCustomCmdFlags()
+
+	if err := cmd.Init(); err != nil {
+		log.Fatal(err)
+	}
+
 	service := micro.NewService(
 		micro.Name("com.thriveglobal.service.poc.pub"),
 		micro.Version("latest"),
+		micro.Broker(azqueue.NewBroker(
+			azqueue.StorageAccountName(storageAccountName),
+			azqueue.StorageAccountKey(storageAccountKey),
+		)),
 	)
 
 	service.Init()
 
-	publisher := micro.NewPublisher("com.thriveglobal.service.poc", service.Client())
+	publisher := micro.NewPublisher(
+		// this is the queue name at azure
+		"sample-queue",
+		service.Client(),
+	)
 
 	for now := range time.Tick(time.Second) {
-		if err := publisher.Publish(context.TODO(), &proto.Message{Say: now.String()}); err != nil {
+		if err := publisher.Publish(context.TODO(), &proto.Message{Say: now.String()}, client.PublishContext(context.Background())); err != nil {
 			log.Fatalf("cannot publish: %s", err)
 		}
 	}
