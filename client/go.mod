2ef8eb21bd3942b3c8c2aff8870dc0799586bbdf
Juan Alvarez <nebiros@gmail.com>
2020-05-28 09:50:55 -0500

    Clean-up ping pong example

--- client/go.mod
+++ client/go.mod
@@ -3,7 +3,6 @@ module github.com/nebiros/poc-go-micro/client
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.0
 	github.com/micro/go-micro/v2 v2.7.0
-	google.golang.org/protobuf v1.22.0
+	github.com/nebiros/poc-go-micro/server v0.0.0-20200528135749-bf67d010f2d6
 )
--- client/main.go
+++ client/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 
-	proto "github.com/nebiros/poc-go-micro/client/proto/client"
+	log "github.com/micro/go-micro/v2/logger"
+
+	proto "github.com/nebiros/poc-go-micro/server/proto/example"
 
 	"github.com/micro/go-micro/v2"
 )
+
+func init() {
+	log.Init(log.WithLevel(log.Level(log.TraceLevel)))
+}
 
 func main() {
 	service := micro.NewService(
@@ -18,12 +22,34 @@ func main() {
 
 	service.Init()
 
-	client := proto.NewClientService("com.thriveglobal.service.poc", service.Client())
+	client := service.Client()
 
-	resp, err := client.Call(context.TODO(), &proto.Request{Name: "Bill 4"})
+	req := client.NewRequest("com.thriveglobal.service.poc", "Example.PingPong", &proto.StreamingRequest{})
+
+	stream, err := client.Stream(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(resp.Msg)
+	for i := 0; i < 10; i++ {
+		if err := stream.Send(&proto.Ping{Stroke: int64(i + 1)}); err != nil {
+			log.Fatal(err)
+		}
+
+		resp := &proto.Pong{}
+
+		if err := stream.Recv(resp); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Infof("Sent ping %v got pong %v\n", i+1, resp.Stroke)
+	}
+
+	if stream.Error() != nil {
+		log.Fatal(err)
+	}
+
+	if err := stream.Close(); err != nil {
+		log.Fatal("cannot close ping pong service: %s", err)
+	}
 }
